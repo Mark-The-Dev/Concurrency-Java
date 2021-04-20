2df8f00c2639be31ef36c9d125ac753c88babdc1
Mark <SwiftTactics@gmail.com>
2021-04-20 19:33:35 -0400

    added a basic writer class for the package

--- src/ProducerAndConsumer/Writer.java
+++ src/ProducerAndConsumer/Writer.java
@@ -1,5 +1,39 @@
 package ProducerAndConsumer;
 
-public class Writer {
+import java.util.Random;
+
+// Basic Writer class.
+
+public class Writer implements Runnable {
     private Message message;
+
+    public Writer(Message message) {
+        this.message = message;
+    }
+
+    public void run(){
+        // haha couldn't help myself here
+        String messages[]= {
+                "Humpty Dumpty sat on a wall",
+                "Humpty Dumpty had a great fall",
+                "All the king's horses and all the king's men",
+                "Couldn't put Humpty together again."
+        };
+
+        Random random = new Random();
+        for ( int i =0; i < messages.length; i++){
+            message.write(messages[i]);
+            try {
+                // sleep to simulate how threading works!
+                Thread.sleep(random.nextInt(2000));
+            } catch (InterruptedException e){
+                e.printStackTrace();
+
+            }
+
+        }
+
+        message.write("Finished");
+    }
+
 }
