87830ee494bd08e87db907b9dbf5ff2b8290845b
Mark <SwiftTactics@gmail.com>
2021-04-19 20:32:32 -0400

    added a thread join and interrupt

--- src/BasicThreads/MainThreads.java
+++ src/BasicThreads/MainThreads.java
@@ -1,7 +1,6 @@
 package BasicThreads;
 
-import static BasicThreads.ThreadColor.ANSI_GREEN;
-import static BasicThreads.ThreadColor.ANSI_PURPLE;
+import static BasicThreads.ThreadColor.*;
 
 public class MainThreads {
 
@@ -22,8 +21,31 @@ public class MainThreads {
             }
         }.start();
 
-        Thread myRunnableThread = new Thread(new MyRunnable());
+        Thread myRunnableThread = new Thread(new MyRunnable(){
+
+            @Override
+            public void run(){
+                System.out.println(ANSI_RED + "Hello form anonymous class's implementation.");
+
+                try {
+                    anotherThread.join();
+                    System.out.println(ANSI_RED + "Another thread terminated, or timed out so I'm running again.");
+
+                } catch (InterruptedException e){
+                    System.out.println(ANSI_RED + "I couldn't wait after all, I was interrupted!");
+                }
+
+            }
+
+        });
+
+
         myRunnableThread.start();
+
+        // can interrupt each thread.
+        //anotherThread.interrupt();
+
+
 
         // this might hit before anotherThread.
         System.out.println(ANSI_PURPLE + "Hello again from the main thread.");
