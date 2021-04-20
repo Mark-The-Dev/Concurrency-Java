64dd2854d71eb5c92e4cd58037ed5db73a0e1896
Mark <SwiftTactics@gmail.com>
2021-04-19 21:00:55 -0400

    added notes about thread variables, stack and heap allocation

--- src/Counter/CounterMain.java
+++ src/Counter/CounterMain.java
@@ -5,6 +5,7 @@ import BasicThreads.ThreadColor;
 public class CounterMain {
     public static void main(String[] args) {
 
+        // Object instances are stored in the heap (Shared among threads unlike the stack.)
         CountDown countDown = new CountDown();
 
         CountDownThread t1 = new CountDownThread(countDown);
@@ -13,6 +14,8 @@ public class CounterMain {
         CountDownThread t2 = new CountDownThread(countDown);
         t2.setName("Thread 2");
 
+        // Thread interference occurs because both threads share the same read object.
+        // More common term is race condition if two or more threads are writing or updating the same resource (it's okay if reading).
         t1.start();
         t2.start();
 
@@ -22,6 +25,9 @@ public class CounterMain {
 
 class CountDown {
     // adding i as a member variable makes the looped i's not duplicated.
+    // When it was defined in the for loop it was allocated in the stack so each object had it's own range of i.
+    // When it is defined as a member variable then it is allocated in the heap which is shared among threads.
+
     private int i;
 
     public void doCountDown(){
@@ -38,7 +44,14 @@ class CountDown {
                 color = ThreadColor.ANSI_GREEN;
         }
 
+
+        // thread could be suspended before decrementing i,
+        // thread could be suspended before checking the conditional,
+        // thread could be suspended before checking i,
+        // thread could be suspended could be suspended after all the conditions are checked but before the print line.
+
         for (i = 10; i> 0; i--){
+            // Thread could be suspended within print line!
             System.out.println(color + Thread.currentThread().getName() + ": i =" + i);
         }
     }
