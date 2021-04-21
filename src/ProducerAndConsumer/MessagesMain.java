d1dde078de949f654109bf08b32b2f1f3c6fe814
Mark <SwiftTactics@gmail.com>
2021-04-20 20:20:01 -0400

    added Reader class

--- src/ProducerAndConsumer/MessagesMain.java
+++ src/ProducerAndConsumer/MessagesMain.java
@@ -3,6 +3,11 @@ package ProducerAndConsumer;
 public class MessagesMain {
     public static void main(String[] args) {
 
+        Message message = new Message();
+        (new Thread(new Writer(message))).start();
+        (new Thread(new Reader(message))).start();
+
+
 
     }
 }
