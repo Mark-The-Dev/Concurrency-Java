7b660e37d3837fa13b98d0eb6447215985d1a69d
Mark <SwiftTactics@gmail.com>
2021-04-22 16:49:14 -0400

    added comments as to why sayHello locks

--- src/DeadLocks/SayHello/SayHelloMain.java
+++ src/DeadLocks/SayHello/SayHelloMain.java
@@ -17,6 +17,21 @@ public class SayHelloMain {
         new Thread(() -> john.sayHello(jane)).start();
 
 
+        // 1. Thread1 acquires the lock on the jane object and enters the sayHello() method.
+        // It prints to the console, then suspends.
+
+        // 2. Thread2 acquires the lock on the john object and enters the sayHello() method.
+        // It prints to the console, then suspends.
+
+        // 3. Thread1 runs again and wants to say hello back to the john object. It tries to call the sayHelloBack() method
+        // using the john object that was passed into the sayHello() method,
+        // but Thread2 is holding the john lock, so Thread1 suspends.
+
+        // 4. Thread2 runs again and wants to say hello back to the jane object. It tries to call the sayHelloBack() method
+        // using the jane object that was passed into the sayHello() method,
+        // but Thread1 is holding the jane lock, so Thread2 suspends.
+
+
     }
 
     static class PolitePerson {
